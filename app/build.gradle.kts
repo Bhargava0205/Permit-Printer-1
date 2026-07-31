bc27cc19397adf02d1d57ab2d28e91a3bb0e3fa0
Bhargava0205 <167779753+Bhargava0205@users.noreply.github.com>
2026-07-31 23:17:49 +0530

    v8

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.example.permitprint"
         minSdk = 24
         targetSdk = 34
-        versionCode = 12
-        versionName = "1.2"
+        versionCode = 16
+        versionName = "1.6"
     }
 
     signingConfigs {
